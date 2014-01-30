bec6afc15b7715a6a27930d3ab69e901b7dcc519
lc29grei <lc29grei@siena.edu>
2014-01-30 16:51:32 -0500

    Initial Commit

---
+++ check.php
@@ -0,0 +1,31 @@
+<?php
+  session_start();
+  $_SESSION = array();
+  if($_SERVER['REQUEST_METHOD'] == "POST") {
+    mysql_connect("localhost","root","");
+    @mysql_select_db("jprep") or die( "Unable to connect to database");
+    $username = mysql_real_escape_string($_POST['username']);
+    $password = mysql_real_escape_string($_POST['password']);
+    $result = mysql_query("SELECT * FROM users WHERE username='$username' AND
+      password='$password'");
+	$accounttype = mysql_result(mysql_query("SELECT type FROM users WHERE username='$username' AND password='$password'"),0)."";
+	
+	
+	mysql_result($result, 2);
+	
+
+    if(mysql_num_rows($result) > 0) {
+      $_SESSION['is_logged_in'] = 1;
+    }
+  }
+
+  if($_SESSION['is_logged_in']==1) {
+	$_SESSION['testvar']=$accounttype;
+	if($accounttype=="student") header("location:student_home.php");
+	else header("location:faculty_home.php");
+  } else {
+    header("location:login.php");
+	session_destroy();
+	$_SESSION = array();
+  }
+?>
---
+++ student_home.php
@@ -0,0 +1,33 @@
+
+<!DOCTYPE html>
+<!--[if lt IE 7]> <html class="lt-ie9 lt-ie8 lt-ie7" lang="en"> <![endif]-->
+<!--[if IE 7]> <html class="lt-ie9 lt-ie8" lang="en"> <![endif]-->
+<!--[if IE 8]> <html class="lt-ie9" lang="en"> <![endif]-->
+<!--[if gt IE 8]><!--><html lang="en"> <!--<![endif]-->
+
+
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+  <title>JPREP Login</title>
+  <link rel="stylesheet" href="css/style_home.css">
+  <!--[if lt IE 9]><script src="//html5shim.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
+</head>
+<body>
+  <section class="container">
+	<div class="header">
+		<!-- Navigation Bar -->
+    	<ul id="nav-bar">
+			<li class="first"><a href="#/home" class="home active">Home</a></li>
+        	<li><a href="#/team" class="team">The Team</a></li>
+        	<li><a href="#/status" class="status">Current Status</a></li>
+			<li><a href="#/documents" class="documents">Documents</a></li>
+			<li><a href="#/notes" class="notes">Meeting Notes</a></li>
+			<li><a href="#/prototype" class="prototype">Prototype</a></li>
+    	</ul><!-- Navigation Bar -->	
+	</div>
+    <h1>Youve logged on to JPREP as a student!</h1>
+  </section><!-- /.container -->
+</body>
+</html>
+
