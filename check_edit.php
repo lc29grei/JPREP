34e0594caaf6548e42d6898d560ed543aff48c98
lc29grei <lc29grei@siena.edu>
2014-02-09 20:19:19 -0500

    Edit profile information

---
+++ check_edit.php
@@ -0,0 +1,88 @@
+<?php
+  session_start();
+  
+  $dbhost = 'localhost';
+  $dbuser = 'root';
+  $dbpass = '';
+  $conn = mysql_connect($dbhost, $dbuser, $dbpass);
+  
+  if($_SERVER['REQUEST_METHOD'] == "POST") {
+    
+    #$new_prefix = mysql_real_escape_string($_POST['prefix']);
+	if($_POST['fname']!=null)
+	{
+		$new_firstname = mysql_real_escape_string($_POST['fname']);
+	}
+	else $new_firstname = $_SESSION['first_name'].'';
+	
+	if($_POST['lname']!=null)
+	{
+		$new_lastname = mysql_real_escape_string($_POST['lname']);
+	}
+	else $new_lastname = $_SESSION['last_name'].'';
+	
+	if($_POST['email']!=null)
+	{
+		$new_username = mysql_real_escape_string($_POST['email']);
+	}
+	else $new_username = $_SESSION['username'].'';
+	
+	if($_POST['password']!=null)
+	{
+		$new_password = mysql_real_escape_string($_POST['password']);
+	}
+	else $new_password = $_SESSION['password'].'';
+	
+	if($_POST['secQ']!=null)
+	{
+		$new_secq = mysql_real_escape_string($_POST['secQ']);
+	}
+	else $new_secq = $_SESSION['secQ'].'';
+	
+	if($_POST['secA']!=null)
+	{
+		$new_seca = mysql_real_escape_string($_POST['secA']);
+	}
+	else $new_seca = $_SESSION['secA'].'';
+	
+	
+	
+	$sql = 'UPDATE users
+			SET 
+			first="'.$new_firstname.'",
+			last="'.$new_lastname.'", 
+			secQ="'.$new_secq.'", 
+			secA="'.$new_seca.'",
+			username="'.$new_username.'",
+			password="'.$new_password.'"
+			WHERE username="'.$_SESSION['username'].'"';
+	mysql_select_db('jprep');
+	$retval = mysql_query( $sql, $conn );
+	if(! $retval )
+	{
+	  die('Could not update data: ' . mysql_error());
+	}
+	echo "Updated data successfully\n";
+	
+	$username = $new_username;
+    $password = $new_password;
+	$accounttype = mysql_result(mysql_query("SELECT type FROM users WHERE username='$username' AND password='$password'"),0)."";
+	$firstName = mysql_result(mysql_query("SELECT first FROM users WHERE username='$username' AND password='$password'"),0)."";
+	$lastName = mysql_result(mysql_query("SELECT last FROM users WHERE username='$username' AND password='$password'"),0)."";
+	$prefix = mysql_result(mysql_query("SELECT prefix FROM users WHERE username='$username' AND password='$password'"),0)."";
+	$secQ = mysql_result(mysql_query("SELECT secQ FROM users WHERE username='$username' AND password='$password'"),0)."";
+	$secA = mysql_result(mysql_query("SELECT secA FROM users WHERE username='$username' AND password='$password'"),0)."";
+	
+	$_SESSION['username']=$username.'';
+	$_SESSION['account_type']=$accounttype.'';
+	$_SESSION['first_name']=$firstName.'';
+	$_SESSION['last_name']=$lastName.'';
+	$_SESSION['prefix']=$prefix.'';
+	$_SESSION['secQ']=$secQ.'';
+	$_SESSION['secA']=$secA.'';
+	$_SESSION['password']=$password.'';
+	mysql_close($conn);
+
+	header("location:home.php");	
+  }
+?>
--- display_profile.php
+++ display_profile.php
@@ -3,18 +3,18 @@ function displayProfile($accounttype)
 {
 	if ($accounttype=="student") {
 		echo'
-			<div class="profile">
-		<a href="?action=editProfile"><u>Edit Profile</u></a>
-		<a href="" style="padding-left:15px;"><u>Change Password</u></a><br>
-			<ul>
-				<li>First Name: '.$_SESSION['first_name'].'</li>
-				<li>Last Name: '.$_SESSION['last_name'].'</li>
-				<li>Email Address: '.$_SESSION['username'].'</li>
-				<li>Password: '.$_SESSION['password'].'</li>
-				<li>Security Question: '.$_SESSION['secQ'].'</li>
-				<li>Security Answer: '.$_SESSION['secA'].'</li>
-			</ul>
-	</div>';
+		<div class="profile">
+			<a href="?action=editProfile"><u>Edit Profile</u></a>
+			<a href="" style="padding-left:15px;"><u>Change Password</u></a><br>
+				<ul>
+					<li>First Name: '.$_SESSION['first_name'].'</li>
+					<li>Last Name: '.$_SESSION['last_name'].'</li>
+					<li>Email Address: '.$_SESSION['username'].'</li>
+					<li>Password: '.$_SESSION['password'].'</li>
+					<li>Security Question: '.$_SESSION['secQ'].'</li>
+					<li>Security Answer: '.$_SESSION['secA'].'</li>
+				</ul>
+		</div>';
 	
 	} else {
 	echo'	
@@ -40,32 +40,33 @@ if(isset($_GET['action']) && $_GET['action'] == 'editProfile'){
 }
 	
 function editProfile($accounttype) {
-	if ($accounttype=="student") {
+		if ($accounttype=="student") {
+			
 			echo'
 			<div class="profile">
-			<ul>
-				<li>First Name: <input type="text" name="fname"></li>
-				<li>Last Name: <input type="text" name="lname"></li>
-				<li>Email Address: <input type="text" name="email"></li>
-				<li>Password: '.$_SESSION['password'].'</li>
-				<li>Security Question: <input type="text" name="secQ"></li>
-				<li>Security Answer: <input type="text" name="secA"></li><br>
-				<input type="submit" value="Submit">
-			</ul>
+				<form method="POST" action="check_edit.php">
+					<p>First Name:<input type="text" name="fname" placeholder="'.$_SESSION['first_name'].'"></p>
+					<p>Last Name:<input type="text" name="lname" placeholder="'.$_SESSION['last_name'].'"></p>
+					<p>Username:<input type="text" name="email" placeholder="'.$_SESSION['username'].'"></p>
+					<p>Password:<input type="text" name="password" placeholder="'.$_SESSION['password'].'"></p>
+					<p>Security Question:<input type="text" name="secQ" placeholder="'.$_SESSION['secQ'].'"></p>
+					<p>Security Answer:<input type="text" name="secA" placeholder="'.$_SESSION['secA'].'"></p>
+					<p class="submit"><input type="submit" name="commit" value="Submit"></p>
+				</form>
 			</div>';
 		} else {
 			echo'
 			<div class="profile">
-			<ul>
-				<li>Prefix: <input type="text" name="prefix"></li>
-				<li>First Name: <input type="text" name="fname"></li>
-				<li>Last Name: <input type="text" name="lname"></li>
-				<li>Email Address: <input type="text" name="email"></li>
-				<li>Password: '.$_SESSION['password'].'</li>
-				<li>Security Question: <input type="text" name="secQ"></li>
-				<li>Security Answer: <input type="text" name="secA"></li><br>
-				<input type="submit" value="Submit">
-			</ul>
+				<form method="POST" action="check_edit.php">
+					<p>First Name:<input type="text" name="prefix" placeholder="'.$_SESSION['[prefix'].'"></p>
+					<p>First Name:<input type="text" name="fname" placeholder="'.$_SESSION['first_name'].'"></p>
+					<p>Last Name:<input type="text" name="lname" placeholder="'.$_SESSION['last_name'].'"></p>
+					<p>Username:<input type="text" name="email" placeholder="'.$_SESSION['username'].'"></p>
+					<p>Password:<input type="text" name="password" placeholder="'.$_SESSION['password'].'"></p>
+					<p>Security Question:<input type="text" name="secQ" placeholder="'.$_SESSION['secQ'].'"></p>
+					<p>Security Answer:<input type="text" name="secA" placeholder="'.$_SESSION['secA'].'"></p>
+					<p class="submit"><input type="submit" name="commit" value="Submit"></p>
+				</form>
 			</div>';
 		}
 }
