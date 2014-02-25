3ece8caab14fa7e6fc028031af28f99c05079553
lc29grei <lc29grei@siena.edu>
2014-02-24 22:53:12 -0500

    Created tables for manage accounts
    Still need to make create new account functional

--- display_manage_accounts.php
+++ display_manage_accounts.php
@@ -3,12 +3,14 @@
 		echo'
 		<div>
 			<ul>
-				<a href=""><li><u>Manage Students</u></li></a>
-				<a href=""><li><u>Manage Faculty</u></li></a>
-				<a href=""><li><u>Manage Course Coordinators</u></li></a>
+				<a href="./manage_students.php"><li><u>Manage Students</u></li></a>
+				<a href="./manage_faculty.php"><li><u>Manage Faculty</u></li></a>
+				<a href="./manage_course_coordinator.php"><li><u>Manage Course Coordinators</u></li></a>
 			</ul>
 		</div>
 		';
 	}
 	
+	
 ?>
+
