d2801d2d6a9eba3748e7d0d256dd4d333d2256ae
lc29grei <lc29grei@siena.edu>
2014-04-07 17:13:32 -0400

    Added functionality to delete course
    Queries database for existing courses. [We need to add a boolean to a
    course whether it is current or past (if we still want to separate
    courses based on current/past). ] Then you can choose a course to delete
    and submit.

--- delete_course.php
+++ delete_course.php
@@ -4,6 +4,16 @@
 	if (!(isset($_SESSION['is_logged_in']) && $_SESSION['is_logged_in']!='')) {
 		header ("Location: login.php");
 	}
+	$dbhost = "localhost";
+  	$dbuser = "root";
+  	$dbpass = "";
+  	$conn = mysql_connect($dbhost, $dbuser, $dbpass);
+	mysql_select_db("jprep");
+	
+	$sectionCountQuery = "SELECT * FROM section";
+	$sectionCountResult = mysql_query($sectionCountQuery);
+	$sectionCountResultRows = mysql_num_rows($sectionCountResult);
+	
 	$currentrole=$_SESSION['currentrole'];
 	$firstname=$_SESSION['first_name'];
    	include 'home_layout.php';
@@ -13,18 +23,56 @@
 			<div>
 			<p style="font-size:12px;">Select the course(s) to be deleted</p>
 			<p style="font-size:16px;"><u>Current Courses</u></p>
-			<table><tr><td>Course 1</td><td></td><td><input type="checkbox"></td></tr>
-					<tr><td>Course 2</td><td></td><td><input type="checkbox"></td></tr>
-					<tr><td>Course 3</td><td></td><td><input type="checkbox"></td></tr></table>
+			<form action="" method="post">
+			<table>';
+			
+			if ($sectionCountResultRows > 0) {
+				while($row=mysql_fetch_array($sectionCountResult)) {
+			
+					echo'<tr><td>'.$row['courseId'].'-'.$row['sectionId'].' '.$row['coursename'].'
+					</td><td></td><td><input name="'.$row['courseId'].$row['sectionId'].'" type="checkbox"></td></tr>';
+				}
+			}	
+			echo'
+			</table>
 			<p style="font-size:16px;"><u>Previous Courses</u></p>
 			<table><tr><td>Course 4</td><td></td><td><input type="checkbox"></td></tr>
 					<tr><td>Course 5</td><td></td><td><input type="checkbox"></td></tr></table>
 					
 			<br>
 			<input type="button" onClick="goBack()" value="Cancel">
-			<input type="button" value="Delete Course(s)">
+			<input type="submit" name="deleteCoursePost" value="Delete Course(s)">
+			</form>
 			
 			</div>';
+			
+			mysql_data_seek( $sectionCountResult, 0 );
+			if(isset($_POST['deleteCoursePost'])){
+				if ($sectionCountResultRows > 0) {
+					while($row=mysql_fetch_array($sectionCountResult)) {
+						if (isset($_POST["".$row['courseId'].$row['sectionId'].""])) {
+							$deleteCourseQuery = "DELETE FROM section where courseId='".$row["courseId"]."' and sectionId='".$row["sectionId"]."'";			
+							//mysql_query($conn,$deleteCourseQuery);
+							//mysql_close($conn);
+							
+							// Perform Query
+							$result = mysql_query($deleteCourseQuery);
+
+							// Check result
+							// This shows the actual query sent to MySQL, and the error. Useful for debugging.
+							if (!$result) {
+								$message  = 'Invalid query: ' . mysql_error() . "\n";
+								$message .= 'Whole query: ' . $deleteCourseQuery;
+								die($message);								
+							}
+							header("Location: home.php");
+						}
+					}
+					
+				}		
+				
+			}
+			
 			
 		#<!-- Manage Accounts tab -->
 			include 'display_manage_accounts.php';
